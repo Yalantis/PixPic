37c4bdb0e8677500e32503a73c5d2e38af1f72bc
Jack Lapin <Athey@MacBook-Air.local>
2016-01-15 18:40:24 +0200

    Main models plus keys

---
+++ P-effect/P-effect/Classes/DTO/PEFPost.swift
@@ -0,0 +1,46 @@
+//
+//  PEFPost.swift
+//  P-effect
+//
+//  Created by Jack Lapin on 15.01.16.
+//  Copyright © 2016 Yalantis. All rights reserved.
+//
+
+var pagination: Int = 0
+let displayLimit = 20
+
+class PEFPost: PFObject {
+    
+    @NSManaged var image: PFFile
+    @NSManaged var user: User?
+    @NSManaged var comment: String?
+    
+    override class func initialize() {
+        var onceToken: dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            self.registerSubclass()
+        }
+    }
+    
+    override class func query() -> PFQuery? {
+        let query = PFQuery(className: PEFPost.parseClassName())
+        //query.cachePolicy = .CacheElseNetwork
+        
+        //TODO: to puzzle out in query.cachePolicy
+        
+        query.includeKey("user")
+        query.orderByDescending("updatedAt")
+        query.limit = displayLimit
+        query.skip = pagination * query.limit
+        return query
+    }
+    
+}
+
+extension PEFPost: PFSubclassing {
+    
+    class func parseClassName() -> String {
+        return "PEFPost"
+    }
+    
+}
--- P-effect/P-effect/Classes/DTO/User.swift
+++ P-effect/P-effect/Classes/DTO/User.swift
@@ -25,39 +25,4 @@ class User: PFUser {
         return PFUser.currentUser() as? User
     }
     
-    func checkIfUsernameExists(completion:(Bool) -> ())  {
-        let query = PFUser.query()?.whereKey("username", equalTo: self.username!)
-        query!.getFirstObjectInBackgroundWithBlock({ (object, error) -> Void in
-            if object != nil {
-                completion(true)
-                print("username exists")
-            } else {
-                completion(false)
-            }
-        })
-    }
-    
-    func checkIfFacebookIdExists(completion:(Bool) -> ())  {
-        let query = PFUser.query()?.whereKey("facebookId", equalTo: self.facebookId!)
-        query!.getFirstObjectInBackgroundWithBlock({ (object, error) -> Void in
-            if object != nil {
-                completion(true)
-                print("facebookId exists")
-            } else {
-                completion(false)
-            }
-        })
-    }
-    
-    func linkOrUnlinkFacebook(completion: (Bool?, NSError?) -> ())  {
-        if PFFacebookUtils.isLinkedWithUser(self) {
-            PFFacebookUtils.unlinkUserInBackground(self, block: { (success, error) -> Void in
-                success ? completion(true, error) : completion(false, error)
-            })
-        } else {
-            PFFacebookUtils.linkUserInBackground(self, withReadPermissions: ["public_profile", "email"], block: { (success, error) -> Void in
-                success ? completion(true, error) : completion(false, error)
-            })
-        }
-    }
 }
