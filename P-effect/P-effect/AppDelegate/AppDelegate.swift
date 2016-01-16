46f54bfd45cf2e13f148d9ebc9a544637cfa750d
Illya Tihonkov <illya.tihonkov@yalantis.com>
2016-01-16 11:44:16 +0200

    CI setup  in progress

--- P-effect/P-effect/AppDelegate/AppDelegate.swift
+++ P-effect/P-effect/AppDelegate/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         return true
     }
 
