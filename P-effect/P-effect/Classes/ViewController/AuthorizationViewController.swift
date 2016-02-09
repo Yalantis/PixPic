2966216cedf8bdba10c8550013e8c41ccf4dfb0f
Jack Lapin <Athey@MacBook-Air.local>
2016-02-09 12:52:57 +0200

    code review

--- P-effect/P-effect/Classes/ViewController/AuthorizationViewController.swift
+++ P-effect/P-effect/Classes/ViewController/AuthorizationViewController.swift
@@ -24,24 +24,22 @@ class AuthorizationViewController: UIViewController {
     private func signInWithFacebook() {
         FBAuthorization.signInWithFacebookInController(
             self,
-            completion: { user, error in
+            completion: { [weak self] user, error in
                 if let error = error {
                     handleError(error as NSError)
                 }
-                guard let user = user as User! else {
-                    self.proceedWithoutAuthorization()
+                guard let user = user as User? else {
+                    self?.proceedWithoutAuthorization()
                     return
                 }
-                UserModel.init(aUser: user).checkIfFacebookIdExists(
-                    { exists in
-                        if exists {
-                            user.passwordSet = false
-                            self.signIn(user)
-                        } else {
-                            self.signUp(user)
-                        }
+                UserModel(aUser: user).checkIfFacebookIdExists { exists in
+                    if exists {
+                        user.passwordSet = false
+                        self?.signIn(user)
+                    } else {
+                        self?.signUp(user)
                     }
-                )
+                }
             }
         )
     }
@@ -57,30 +55,29 @@ class AuthorizationViewController: UIViewController {
             { user, error in
                 if let error = error {
                     handleError(error as NSError)
-                } else if let user = user {
-                    let user = UserModel.init(aUser: user)
-                    user.user.facebookId = userWithFB.facebookId
-                    user.user.username = userWithFB.username
-                    user.user.email = userWithFB.email
-                    user.user.avatar = userWithFB.avatar
-                    user.user.saveInBackgroundWithBlock(
-                        { succes, error in
-                            if let error = error {
-                                print(error)
-                            } else {
-                                print("NEW DATAA FOR LINKED USER")
-                                let installation = PFInstallation.currentInstallation()
-                                installation["user"] = user.user
-                                installation.saveInBackground()
-                            }
-                        }
-                    )
-                    print("SIGNING UP!!!  with ", user.user.username)
-                    Router.sharedRouter().showHome(animated: true)
-                } else {
-                    print("unknown trouble while signing IN")
                 }
-                self.view.hideToastActivity()
+                guard let user = user else {
+                    self.view.hideToastActivity()
+                    print("unknown trouble while signing IN")
+                    return
+                }
+                let userModel = UserModel(aUser: user)
+                userModel.user.facebookId = userWithFB.facebookId
+                userModel.user.username = userWithFB.username
+                userModel.user.email = userWithFB.email
+                userModel.user.avatar = userWithFB.avatar
+                userModel.user.saveInBackgroundWithBlock { _, error in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        print("NEW DATAA FOR LINKED USER")
+                        let installation = PFInstallation.currentInstallation()
+                        installation["user"] = userModel.user
+                        installation.saveInBackground()
+                    }
+                }
+                print("SIGNING UP!!!  with ", userModel.user.username)
+                Router.sharedRouter().showHome(animated: true)
             }
         )
     }
@@ -93,23 +90,25 @@ class AuthorizationViewController: UIViewController {
                 [weak self] user, error in
                 if let _ = error {
                     ExceptionHandler.handle(Exception.InvalidSessionToken)
-                } else {
-                    let user = UserModel.init(aUser: user as! User)
-                    user.linkIfUnlinkFacebook(
-                        { error in
-                            if let error = error {
-                                handleError(error)
-                            } else {
-                                print("already linked!")
-                                let installation = PFInstallation.currentInstallation()
-                                installation["user"] = user.user
-                                installation.saveInBackground()
-                            }
-                        }
-                    )
                 }
-                self?.view.hideToastActivity()
-                Router.sharedRouter().showHome(animated: true)
+                guard let user = user as? User else {
+                    self?.view.hideToastActivity()
+                    Router.sharedRouter().showHome(animated: true)
+                    return
+                }
+                let userModel = UserModel(aUser: user)
+                userModel.linkIfUnlinkFacebook { error in
+                    if let error = error {
+                        handleError(error)
+                    } else {
+                        print("already linked!")
+                        let installation = PFInstallation.currentInstallation()
+                        installation["user"] = userModel.user
+                        installation.saveInBackground()
+                        self?.view.hideToastActivity()
+                        Router.sharedRouter().showHome(animated: true)
+                    }
+                }
             }
         )
     }
