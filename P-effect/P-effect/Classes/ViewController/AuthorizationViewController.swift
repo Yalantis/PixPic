d9887456400c74071be99d4dd436e8278661a9d5
Jack Lapin <Athey@MacBook-Air.local>
2016-02-09 13:01:17 +0200

    code review

--- P-effect/P-effect/Classes/ViewController/AuthorizationViewController.swift
+++ P-effect/P-effect/Classes/ViewController/AuthorizationViewController.swift
@@ -51,44 +51,41 @@ class AuthorizationViewController: UIViewController {
     
     private func signUp(user: User) {
         let userWithFB = user
-        FBAuthorization.signInWithPermission(
-            { [weak self] user, error in
-                if let error = error {
-                    handleError(error as NSError)
-                }
-                guard let user = user else {
-                    self?.view.hideToastActivity()
-                    print("unknown trouble while signing IN")
-                    return
-                }
-                let userModel = UserModel(aUser: user)
-                userModel.user.facebookId = userWithFB.facebookId
-                userModel.user.username = userWithFB.username
-                userModel.user.email = userWithFB.email
-                userModel.user.avatar = userWithFB.avatar
-                userModel.user.saveInBackgroundWithBlock { _, error in
-                    if let error = error {
-                        print(error)
-                    } else {
-                        print("NEW DATAA FOR LINKED USER")
-                        let installation = PFInstallation.currentInstallation()
-                        installation["user"] = userModel.user
-                        installation.saveInBackground()
-                    }
-                }
-                print("SIGNING UP!!!  with ", userModel.user.username)
-                self?.view.hideToastActivity()
-                Router.sharedRouter().showHome(animated: true)
+        FBAuthorization.signInWithPermission { [weak self] user, error in
+            if let error = error {
+                handleError(error as NSError)
             }
-        )
+            guard let user = user else {
+                self?.view.hideToastActivity()
+                print("unknown trouble while signing IN")
+                return
+            }
+            let userModel = UserModel(aUser: user)
+            userModel.user.facebookId = userWithFB.facebookId
+            userModel.user.username = userWithFB.username
+            userModel.user.email = userWithFB.email
+            userModel.user.avatar = userWithFB.avatar
+            userModel.user.saveInBackgroundWithBlock { _, error in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("NEW DATAA FOR LINKED USER")
+                    let installation = PFInstallation.currentInstallation()
+                    installation["user"] = userModel.user
+                    installation.saveInBackground()
+                }
+            }
+            print("SIGNING UP!!!  with ", userModel.user.username)
+            self?.view.hideToastActivity()
+            Router.sharedRouter().showHome(animated: true)
+        }
     }
     
     private func signIn(user: User) {
         let token = FBSDKAccessToken.currentAccessToken()
         PFFacebookUtils.logInInBackgroundWithAccessToken(
             token,
-            block: {
-                [weak self] user, error in
+            block: { [weak self] user, error in
                 if let _ = error {
                     ExceptionHandler.handle(Exception.InvalidSessionToken)
                 }
