ff123507a5fe0c8a39372e538921c479625e0398
Jack Lapin <Athey@MacBook-Air.local>
2016-02-29 15:45:53 +0200

    code review

--- P-effect/P-effect/Misc/Categories/UIActivityIndicatorView + UIView.swift
+++ P-effect/P-effect/Misc/Categories/UIActivityIndicatorView + UIView.swift
@@ -11,13 +11,11 @@ import UIKit
 extension UIActivityIndicatorView {
     
     func addActivityIndicatorOn(view view: UIView) -> UIActivityIndicatorView {
-        
         let indicator = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
         indicator.center = CGPointMake(view.frame.size.height/2, view.frame.size.width/2)
         indicator.hidesWhenStopped = true
         indicator.startAnimating()
         view.addSubview(indicator)
-        
         return indicator
     }
     
