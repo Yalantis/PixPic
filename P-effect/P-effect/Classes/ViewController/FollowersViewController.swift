d111c3a4da756ec5f3f0898e63f35045adeee806
anna.zdor <anna.zdor@yalantis.com>
2016-03-03 15:29:20 +0200

    Setup adapter in FolloversVC

--- P-effect/P-effect/Classes/ViewController/FollowersViewController.swift
+++ P-effect/P-effect/Classes/ViewController/FollowersViewController.swift
@@ -11,15 +11,31 @@ import UIKit
 class FollowersViewController: UIViewController {
     
     static let storyboardName = Constants.Storyboard.Profile
+    private lazy var followerAdapter = FollowerAdapter()
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupTableView()
+        setupAdapter()
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
         tableView.registerNib(FollowerViewCell.cellNib, forCellReuseIdentifier: FollowerViewCell.identifier)
     }
-
     
+    private func setupAdapter() {
+        tableView.dataSource = followerAdapter
+    }
+    
+}
 
+extension FollowersViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
